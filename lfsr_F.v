6f9a1e26fa7d296f9b23e4e4a6d670cdb664e512
William-SSAU <70592785+William-SSAU@users.noreply.github.com>
2020-09-01 19:51:24 +0400

    Add files via upload

---
+++ lfsr_F.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+module lfsr #(
+    parameter WIDTH  = 16,
+              TAPS   = 16'b1000000001011,
+              INVERT = 0
+             )
+(
+    input                    clk,
+    input                    reset,
+    input                    enable,
+    output reg [WIDTH - 1:0] out
+);
+    wire feedback = (^(out & TAPS)) ^ INVERT;
+    always @(posedge clk or negedge reset)
+        if (~reset) out <= { { WIDTH - 1 { 1'b0 } }, 1'b1 };
+        else if (enable) out <= { feedback, out [WIDTH - 1:1] };
+endmodule
